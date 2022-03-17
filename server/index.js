fe3a422f2f7eb6672adae74fad9114a98a7c9f7c
oasiao <74007996+oasiao@users.noreply.github.com>
2022-03-17 12:47:00 +0100

    modified index.js file for the server III

--- server/index.js
+++ server/index.js
@@ -38,15 +38,15 @@ server.listen(port, hostname, () => {
 app.listen(port, () => console.log(`Server started on port ${port}`));*/
 
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/home', (req, res) => {
+    res.send('Hello World!');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
 
