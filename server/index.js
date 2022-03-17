2abdb4bb41996f8026debe28f30dcc13dd815da8
oasiao <74007996+oasiao@users.noreply.github.com>
2022-03-17 12:42:05 +0100

    modified index.js file for the server II

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -34,4 +35,18 @@ server.listen(port, hostname, () => {
 });
 
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));*/
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
