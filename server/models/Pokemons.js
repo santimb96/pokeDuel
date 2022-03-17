c3674765516441b292ca0e1173933b05603bdede
Santiago <Martnez>
2022-03-17 18:08:50 +0100

    scrapping data hecho con todoslos pokémons

--- server/models/Pokemons.js
+++ server/models/Pokemons.js
@@ -4,6 +4,9 @@ const Int32 = require("mongoose-int32").loadType(mongoose);
 const schema = mongoose.Schema({
     name: String,
     level: Int32,
+    type: String,
+    speed: Int32,
+    img: String
 })
 
 module.exports = mongoose.model("Pokemons", schema)
