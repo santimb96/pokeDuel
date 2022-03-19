d111c38698e00012c8fd6af77c59e42f87b62406
Santiago <Martnez>
2022-03-20 00:30:39 +0100

    Pokemons.js models editado nuevos campos añadidos

--- server/models/Pokemons.js
+++ server/models/Pokemons.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 const Int32 = require("mongoose-int32").loadType(mongoose);
 
 const schema = mongoose.Schema({
+    _id: Int32,
     name: String,
     level: Int32,
     type: String,
     speed: Int32,
-    img: String
+    img: String,
+    img_game: String
 })
 
 module.exports = mongoose.model("Pokemons", schema)
