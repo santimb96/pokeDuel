394c6ad5fa0f5e4cf7e6f81a2254204699a2a792
oasiao <74007996+oasiao@users.noreply.github.com>
2022-04-14 11:48:30 +0200

    Log in system templates (in a basic way)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 //components
 import {IndexComponent} from "./components/index/index.component";
+import {CreateComponent} from "./components/users/create/create.component";
+import {LogInComponent} from "./components/users/log-in/log-in.component";
 
 const routes: Routes = [
-  {path:'',component:IndexComponent}
+  { path: '', component: IndexComponent },
+  { path: 'new-account', component: CreateComponent }, //create a new user
+  { path: 'log-in', component: LogInComponent }, //logIn user
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,21 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
+
+//components
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CreateComponent } from './components/users/create/create.component';
+import { LogInComponent } from './components/users/log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CreateComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
