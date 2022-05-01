78f70cbf9c620c0aaf832b89eb329062c88601ae
Santiago <Martnez>
2022-05-01 17:33:30 +0200

    multer added on user create and user update

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 
 const router = express.Router();
 
 router
   .get("/", userController.getAll)
   .get("/:id", userController.findId)
-  .post("/", userController.create)
-  .put("/:id", userController.updateById)
+  .post("/", upload.single('avatar'), userController.create)
+  .put("/:id", upload.single('avatar'), userController.updateById)
   .delete("/:id", userController.deleteById)
 
   .post('/login', userController.login)
